cce10c963e4191e486d93ed48d9b03a92fe6beec
Chase Slattery <chs6298@rit.edu>
2017-02-11 17:20:09 -0500

    Added projectile creation/destruction

--- Assets/Scripts/DestroyedProjectile.cs
+++ Assets/Scripts/DestroyedProjectile.cs
@@ -7,13 +7,30 @@ public class DestroyedProjectile : MonoBehaviour {
     public float aliveTime;
 
 	// Use this for initialization
+   
 	void Awake ()
     {
         Destroy(gameObject, aliveTime);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
-}
+    void OnCollisionEnter2D()
+    {
+       Debug.Log("Collision");
+
+
+        Invoke("destroyProjectile", .1f);
+    }
+    void destroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+    
+    
+      
+    }
+
--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -7,14 +7,18 @@ public class ProjectileController : MonoBehaviour {
 
     Rigidbody2D projectileRB;
     public float rocketSpeed;
+  
 
 	// Use this for initialization
 	void Awake ()
     {
         //allows reference to rigidbody
         projectileRB = GetComponent<Rigidbody2D>();
+        
+
         //travels in x direction only
         projectileRB.AddForce(new Vector2(1, 0) * rocketSpeed,ForceMode2D.Impulse);
+       
 
 	}
 	
@@ -23,4 +27,5 @@ public class ProjectileController : MonoBehaviour {
     {
 		
 	}
+  
 }
