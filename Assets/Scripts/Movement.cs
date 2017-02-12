a189834ce4846d47c21a0097a20e87b07c438b9c
Chase Slattery <chs6298@rit.edu>
2017-02-11 21:53:52 -0500

    Created Spawner needed for targets

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        new Vector2(4, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
