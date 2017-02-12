c9b2fbe1015fded4c29c5c1e7574eaba3c3a8874
Chase Slattery <chs6298@rit.edu>
2017-02-11 22:49:06 -0500

    Added Basic Starting Gui

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public void NewGameBtn(string newGamelevel)
+    {
+        SceneManager.LoadScene(newGamelevel);
+    }
+	
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    private states state;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (state == states.playGame)
+        {
+            
+        }
+
+        else if (state == states.MainMenu)
+        {
+
+        }
+        else if (state == states.Exit)
+        {
+            return;
+        }
+		
+	}
+    enum  states
+    {
+        playGame,
+        MainMenu,
+        Exit
+    }
+}
